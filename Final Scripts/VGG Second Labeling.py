e66056e06fc55e3a4a47b787b3e6c1c9d1f1e346
Menna Mohie <mennaxmohiex@gmail.com>
2020-08-15 02:26:24 +0200

    Update VGG Second Labeling.py

--- Final Scripts/VGG Second Labeling.py
+++ Final Scripts/VGG Second Labeling.py
@@ -2,15 +2,15 @@ import glob
 import os
 import numpy as np
 
-#LESSA EL PATHS
-
-dataset_path = '/content/gdrive/My Drive/Team\'s Drive/Graduation Project/Dataset_VGG'
-train_path = '/content/gdrive/My Drive/Team\'s Drive/Graduation Project/Dataset_VGG/train'
+current_dir = os.getcwd()
+dataset_path = current_dir + '/Dataset_VGG'
+train_path = dataset_path + '/Train'
 test_path = '/content/gdrive/My Drive/Team\'s Drive/Graduation Project/Dataset_VGG/test'
-labels_path = '/content/gdrive/My Drive/Team\'s Drive/Graduation Project/VGG_Initial_Labels'
+labels_path = dataset_path + '/initial_labels'
 
-from google.colab import drive
-drive.mount('/content/gdrive')
+# from google.colab import drive
+# drive.mount('/content/gdrive')
+
 
 def get_inner_paths(path):
     inner_paths = glob.glob(path + '/*')
@@ -18,8 +18,10 @@ def get_inner_paths(path):
     inner_names = [os.path.basename(inner_path) for inner_path in inner_paths]
     return inner_paths, inner_names
 
+
 truthful_labels = np.load(labels_path + '/truthful_labels.npy')
 deceptive_labels = np.load(labels_path + '/deceptive_labels.npy')
+
 
 def get_new_labels(path):
     _, video_names = get_inner_paths(path)
@@ -33,6 +35,7 @@ def get_new_labels(path):
             video_num = int(video_name[9:]) - 1
             labels.append(deceptive_labels[video_num])
     return labels
+
 
 train_labels = get_new_labels(train_path)
 np.save(dataset_path + '/train_labels.npy', np.asarray(train_labels))
